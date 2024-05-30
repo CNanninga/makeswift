de04f209b228bd635115c2113e48dbf77a49b9c4
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-05-30 09:54:50 -0500

    Add Makeswift properties to Team component, adjust the way fade in/out works

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -1,11 +1,47 @@
 'use client'
 
-import { useState, forwardRef, Ref } from "react"
+import { useState, forwardRef, Ref, ReactNode, CSSProperties, useRef } from "react"
+import clsx from 'clsx'
 
-export const Team = forwardRef(({}, ref: Ref<HTMLDivElement>) => {
-  const [activeMember, setActiveMember] = useState(0);
+type Member = {
+  name?: string;
+  title?: string;
+  image?: string;
+  content: ReactNode;
+}
 
-  const members = [
+type Props = {
+  members: Member[];
+  itemsPerRow?: number;
+  mobileItemsPerRow?: number;
+  className?: string;
+}
+
+export const Team = forwardRef((
+  { members, className, itemsPerRow = 3, mobileItemsPerRow = 2 }: Props, 
+  ref: Ref<HTMLDivElement>
+) => {
+  const fadeInDuration = 500
+  const [activeMember, setActiveMember] = useState(0)
+  const [visibleMembers, setVisibleMembers] = useState([0])
+
+  const changeActiveMember = (index:number) => {
+    const prevActiveIndex = activeMember
+
+    const newVisibleMembers = [...visibleMembers, index]
+
+    setVisibleMembers(newVisibleMembers)
+
+    setTimeout(() => {
+      setActiveMember(index)
+
+      setTimeout(() => {
+        setVisibleMembers(newVisibleMembers.filter(thisIndex => thisIndex !== prevActiveIndex))
+      }, fadeInDuration * 1.5)
+    }, 10)
+  }
+
+  const fakeMembers = [
     {
       img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
       name: "Leslie Alexander",
@@ -117,24 +153,32 @@ export const Team = forwardRef(({}, ref: Ref<HTMLDivElement>) => {
   ]
 
   return (
-    <div className="m-8" ref={ref}>
+    <div className={clsx("m-8 w-full", className)} ref={ref}>
       <div className="relative h-96">
-        {members.map((member, index) => (
-          <div key={index} 
-            className={`absolute ${index !== activeMember ? 'opacity-0' : 'opacity-100'}
-              transition-opacity duration-500`}
-            dangerouslySetInnerHTML={{__html: member.content}}>
-          </div>
-        ))}
+        {members.map((member, index) => {
+          if (!visibleMembers.includes(index)) return null
+
+          return (
+            <div key={index} 
+              className={`absolute ${index !== activeMember ? 'opacity-0' : 'opacity-100'}
+                transition-opacity duration-${fadeInDuration} w-full`}>
+                {member.content}
+            </div>
+          )
+        })}
       </div>
       <div className="p-4">
-        <ul className="grid grid-cols-4 gap-x-4 gap-y-8 px-16 justify-items-center">
+        <ul className={`grid grid-cols-[var(--mobileItemsPerRow)] lg:grid-cols-[var(--itemsPerRow)] gap-x-4 gap-y-8 px-16 justify-items-center`}
+          style={{ 
+            "--itemsPerRow": `repeat(${itemsPerRow}, minmax(0, 1fr))`,
+            "--mobileItemsPerRow": `repeat(${mobileItemsPerRow}, minmax(0, 1fr))`, 
+          } as CSSProperties}>
           {members.map((member, index) => (
             <li key={index} className={`max-w-48 text-center 
               border ${index === activeMember ? "border-blue-800" : "border-transparent"} border-2 
               p-2 rounded-md cursor-pointer transition-colors duration-300`}
-              onClick={() => setActiveMember(index)}>
-              <img className="rounded-full mx-auto" src={member.img}
+              onClick={() => changeActiveMember(index)}>
+              <img className="rounded-full mx-auto" src={member.image}
                 style={{maxWidth: "60%"}} />
               <h3 className="text-sm font-bold">{member.name}</h3>
               <p className="text-sm">{member.title}</p>
